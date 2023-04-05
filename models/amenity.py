578a4c7b32ecf9bb9e754389fc9d4ce16d24d503
Nurudeen010 <nurudeenolamide010@gmail.com>
2023-04-05 14:24:59 -0700

    Updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Amenity Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import backref
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class handles all application amenities"""
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('PlaceAmenity',
+                                       backref='amenities',
+                                       cascade='delete')
+    else:
+        name = ''
